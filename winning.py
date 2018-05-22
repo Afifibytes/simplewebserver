f5325f795a9249784c68a13b0131c96b235bc5c8
Mohamad Afifi <mo7amad3fifi@gmail.com>
2018-05-22 05:08:58 +0200

    fixing bug in winning function

--- winning.py
+++ winning.py
@@ -1,15 +1,19 @@
 def winning_type(numbersList):
     """determine the winning type for a list of integers"""
+    if numbersList == []:
+        return "No items"
+
     items = []
     counter = 0
     for item in numbersList:
         if item not in items:
             items.append(item)
+        else:
             counter += 1
 
     if counter == 1:
-        return "big win"
-    elif counter == 2:
         return "small win"
-    elif counter == 3:
+    elif counter == 2:
+        return "big win"
+    else:
         return "no win"
